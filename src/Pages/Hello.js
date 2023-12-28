cb228a65c77a72fd30c43cb30dff768ffc216956
Nitesh Giri <ggiri@chirpn.com>
2023-12-28 19:23:43 +0530

    Latest

--- src/Pages/Hello.js
+++ src/Pages/Hello.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 function Hello() {
+  const currentURL = window.location.href;
+  const urlParams = new URLSearchParams(currentURL);
+  const shopParam = urlParams.get("shop");
+
+  let extractedShop = "";
+  if (shopParam) {
+    const matches = shopParam.match(/^([^\.]+)/);
+    if (matches && matches.length > 0) {
+      extractedShop = matches[0];
+    }
+  }
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>This is React App.</p>
+      <p>The Current shop is : {extractedShop}</p>
     </div>
   );
 }
